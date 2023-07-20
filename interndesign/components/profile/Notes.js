1a4d11708968d579ea077bcf7d5c2fcd9aa569d6
mon842 <sayakbasak842@gmail.com>
2023-07-20 19:31:01 +0530

    Update Notes.js

--- interndesign/components/profile/Notes.js
+++ interndesign/components/profile/Notes.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Notes = () => {
   return (
-    <div className='mx-6 my-5 border-b-2 border-gray-300 '>
+    <div className='mx-6 my-7 border-b-2 border-gray-300 '>
         <p className='my-1 text-gray-500 text-xs '>2020-08-07</p>
         <p className='my-1 font-bold text-xs'>wihdrawal transfer to Bank-XXXX</p>
     </div>
